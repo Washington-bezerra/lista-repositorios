b3fe99d2921238dd1212686b4f5f8ba3336f00b5
washington-bezerra <luiswashington00@gmail.com>
2021-12-21 11:04:53 -0300

    remoção do print

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ name_user = str(input("Digite o seu name user do git: "))
 r = requests.get(f'https://api.github.com/users/{name_user}/repos')
 r = r.json()
 
-print(r)
 print(f'{"Nome repositorio":<25}|{"Link repositorio":<70}|')
 for c in r:
 
